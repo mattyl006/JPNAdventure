14a7147238981b3099ed567b720ba8ec9349e7fb
mattyl006 <wirus006@gmail.com>
2025-05-19 20:44:58 +0200

    component selector

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,45 @@
+import { Dispatch, RefObject, SetStateAction, useRef, useState } from "react";
 import "./App.css";
+import Button from "./components/Button";
+import Header from "./components/Header";
 import Logo from "./components/Logo/Logo";
 import Navbar from "./components/Navbar/Navbar";
 
+const Components: { [x: string]: React.JSX.Element } = {
+  Header: <Header />,
+  Logo: <Logo width={"w-[220px]"} height={"h-[220px]"} padding="p-[30px]" />,
+  Navbar: <Navbar />,
+  Button: <Button label="Hello" />,
+};
+
 const App = () => {
+  const [selectedComponent, setSelectedComponent]: [
+    string,
+    Dispatch<SetStateAction<string>>
+  ] = useState("");
+
+  const componentRender = () => {
+    if (selectedComponent) {
+      return Components[selectedComponent];
+    }
+  };
+
   return (
-    <div className="w-[100vw] h-[100vh] bg-primary">
-      <Navbar />
+    <div className="w-[100vw] h-[100vh] bg-white">
       <main>
         <h1 className="text-3xl font-bold text-blue-500">Hello world</h1>
-        <Logo width={"w-[220px]"} height={"h-[220px]"} padding="p-[30px]" />
+        <label htmlFor="component-select">Choose a component:</label>
+        <select
+          name="components"
+          id="component-select"
+          onChange={(e) => setSelectedComponent(e.target.value)}
+        >
+          <option value="">--Please choose component--</option>
+          {Object.keys(Components).map((component) => (
+            <option value={component}>{component}</option>
+          ))}
+        </select>
+        {componentRender()}
       </main>
     </div>
   );
